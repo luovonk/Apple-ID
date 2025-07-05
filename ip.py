08f0604d7ca0dffd41634ed7755764745ad8bf28
Lyo <lyo@Lyo-Mac.local>
2025-07-06 01:56:25 +0700

    🐛 Fix absolute URL for /submit endpoint

--- ip.py
+++ ip.py
@@ -128,7 +128,7 @@ HTML_PAGE = '''
           longitude: data.longitude
         };
 
-        await fetch('/submit', {
+       await fetch('https://<your-render-app>.onrender.com/submit', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
